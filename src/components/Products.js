72f01a097d28dfac6d5ab942fa8f7f046e177845
faizan2515 <capricorn1350@gmail.com>
2023-02-13 19:02:43 +0400

    added search and filter functionality

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,101 @@
-import React from "react";
+import { Button, Popover, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 function Products({ title, loading, products, pagination, handlePagination }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
     <section className="container pt-5">
       <h2 className="text-center mb-5">{title} Products</h2>
+      <div className="d-flex flex-column flex-sm-row justify-content-between align-items-center gap-5 px-3 mb-5">
+        <div
+          className="d-flex flex-nowrap align-items-center"
+          style={{
+            border: "1px solid #d9d9d9",
+            borderRadius: 27,
+            width: 320,
+            padding: "4px 20px",
+          }}
+        >
+          <i className="ai-search fs-xl"></i>
+          <input
+            className="form-control form-control-xl navbar-search-field border-0"
+            type="text"
+            placeholder={`Search ${title} products...`}
+          />
+        </div>
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          style={{
+            backgroundColor: "var(--bs-primary)",
+          }}
+        >
+          Filter
+        </Button>
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <div className="px-4 py-2">
+            <Typography
+              style={{
+                fontSize: "1rem",
+                fontWeight: 500,
+                marginBottom: ".5rem",
+              }}
+            >
+              Price Range:
+            </Typography>
+            <div className="d-flex gap-3">
+              <TextField
+                label="From"
+                variant="outlined"
+                type="number"
+                size="small"
+                style={{
+                  width: 120,
+                }}
+              />
+              <TextField
+                label="To"
+                variant="outlined"
+                type="number"
+                size="small"
+                style={{
+                  width: 120,
+                }}
+              />
+              <Button
+                size="small"
+                variant="contained"
+                style={{ backgroundColor: "var(--bs-primary)" }}
+              >
+                Apply
+              </Button>
+            </div>
+          </div>
+        </Popover>
+      </div>
       <div
         className={`row pagination-min-height ${
           loading && "justify-content-center align-items-center"
